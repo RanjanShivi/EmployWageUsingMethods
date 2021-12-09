5b03515ca5a111768778dc5f43925b9523f3d951
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-09 23:35:03 +0530

    Caluculate wages till condition of maximum work hours or maximum days for a month is reached

--- EmployWageUsingMethods.java
+++ EmployWageUsingMethods.java
@@ -3,6 +3,7 @@ public class EmployWageUsingMethods {
     public static final int full_time=2;
     public static final int emp_rate=20;
     public static final int work_day=20;
+    public static final int max_hrs=100;
 
     public static int attendance() {
         int check = (int) Math.floor(Math.random() * 10) % 3;
@@ -25,18 +26,25 @@ public class EmployWageUsingMethods {
     }
 
     public static void main(String[] args) {
-        int emp_attendance=0, emp_hoursPerDay=0, emp_wage=0, tot_emp_wage=0;
+        int emp_attendance=0, emp_hoursPerDay=0, tot_emp_hours=0, day=0, emp_wage=0, tot_emp_wage=0;
 
-        for (int day=0; day<work_day; day++)
+        while(tot_emp_hours < max_hrs && day < work_day)
         {
             emp_attendance = attendance();
             System.out.println("Employ Attendance Status=" + emp_attendance);
 
             emp_hoursPerDay = calEmpHours(emp_attendance);
+            System.out.println("Day:" + day + " Employ hour per day= " + emp_hoursPerDay );
+
+            day++;
+            tot_emp_hours += emp_hoursPerDay;
             emp_wage = emp_hoursPerDay * emp_rate;
             System.out.println("Employ wage per day = " + emp_wage);
             tot_emp_wage +=emp_wage;
+
+
         }
+        System.out.println("total work day= " + day + " total work hours= " + tot_emp_hours);
         System.out.println("Total Employ wage= " + tot_emp_wage);
     }
 }
