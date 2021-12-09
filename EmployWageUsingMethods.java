0537e68680749cf382fefce7b6a03a12cc912a98
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-09 21:54:05 +0530

    Calculate Daily Employee Wage

--- EmployWageUsingMethods.java
+++ EmployWageUsingMethods.java
@@ -1,19 +1,32 @@
 public class EmployWageUsingMethods {
-    public static final int full_time=1;
+    public static final int full_time = 1;
+    public static final int emp_rate = 20;
 
     public static int attendance() {
         int check = (int) Math.floor(Math.random() * 10) % 3;
         return check;
     }
-    public static void main(String[] args) {
-        int emp_attendance = attendance();
-        System.out.println("Employ Attendance Status="+ emp_attendance);
 
-        if((emp_attendance==full_time))
-            System.out.println("Employ Present");
+    public static int calEmpHours( int attend_status) {
+        int emp_hrs;
+        if (attend_status == full_time)
+            emp_hrs = 8;
         else
-            System.out.println("Employ Absent");
-
+            emp_hrs = 0;
+        return emp_hrs;
     }
 
+    public static void main(String[] args) {
+        int emp_attendance = attendance();
+        System.out.println("Employ Attendance Status=" + emp_attendance);
+
+        int emp_hoursPerDay = calEmpHours(emp_attendance);
+        int emp_wage = emp_hoursPerDay * emp_rate;
+        System.out.println("Employ wage= " + emp_wage);
+    }
 }
+
+
+
+
+
