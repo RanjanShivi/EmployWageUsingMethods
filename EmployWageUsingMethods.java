0d423e704dcf710ef4c472054cb798475c6e86d1
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-09 21:33:07 +0530

    Check whether employee is present or not

---
+++ EmployWageUsingMethods.java
@@ -0,0 +1,19 @@
+public class EmployWageUsingMethods {
+    public static final int full_time=1;
+
+    public static int attendance() {
+        int check = (int) Math.floor(Math.random() * 10) % 3;
+        return check;
+    }
+    public static void main(String[] args) {
+        int emp_attendance = attendance();
+        System.out.println("Employ Attendance Status="+ emp_attendance);
+
+        if((emp_attendance==full_time))
+            System.out.println("Employ Present");
+        else
+            System.out.println("Employ Absent");
+
+    }
+
+}
