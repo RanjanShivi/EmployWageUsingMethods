591c012a9c85127314824515d7ae9cfc0b01a96d
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-09 22:58:28 +0530

    Calculate wages for a month assuming 20 work days in a month

--- EmployWageUsingMethods.java
+++ EmployWageUsingMethods.java
@@ -2,6 +2,7 @@ public class EmployWageUsingMethods {
     public static final int part_time=1;
     public static final int full_time=2;
     public static final int emp_rate=20;
+    public static final int work_day=20;
 
     public static int attendance() {
         int check = (int) Math.floor(Math.random() * 10) % 3;
@@ -24,13 +25,19 @@ public class EmployWageUsingMethods {
     }
 
     public static void main(String[] args) {
-        int emp_hoursPerDay=0, emp_wage=0;
-        int emp_attendance = attendance();
-        System.out.println("Employ Attendance Status=" + emp_attendance);
+        int emp_attendance=0, emp_hoursPerDay=0, emp_wage=0, tot_emp_wage=0;
 
-        emp_hoursPerDay = calEmpHours(emp_attendance);
-        emp_wage = emp_hoursPerDay * emp_rate;
-        System.out.println("Employ wage= " + emp_wage);
+        for (int day=0; day<work_day; day++)
+        {
+            emp_attendance = attendance();
+            System.out.println("Employ Attendance Status=" + emp_attendance);
+
+            emp_hoursPerDay = calEmpHours(emp_attendance);
+            emp_wage = emp_hoursPerDay * emp_rate;
+            System.out.println("Employ wage per day = " + emp_wage);
+            tot_emp_wage +=emp_wage;
+        }
+        System.out.println("Total Employ wage= " + tot_emp_wage);
     }
 }
 
